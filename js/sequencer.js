da3ca085131e7bfd211791104d4dd092b84b1866
Leandro Moraes de Alecrim <136747118+tjsp-lalecrim@users.noreply.github.com>
2023-07-20 09:46:25 -0300

    Feature/automatic interval (#6)
    * countdown to start +
    cancel speech when skipping step
    
    * remove "get ready" and "rest" from exercises-list
    
    * Add interval property to exercise list
    
    * use interval step in countdown
    
    * add interval info on exercises page
    
    * change "start" speech to countdown
    
    * bugfix: add interval if exists

--- js/sequencer.js
+++ js/sequencer.js
@@ -19,6 +19,8 @@ window.onload = function () {
   let currStep = 0;
   let timer = null;
   let paused = false;
+  let speech = new SpeechSynthesisUtterance();
+  speech.lang = "en-US";
 
   // Get workout and exercises
   const currWorkout = workouts.find(function (workout) {
@@ -34,8 +36,7 @@ window.onload = function () {
   // Text to speech
   const readText = (text) => {
     if (!("speechSynthesis" in window)) return;
-    speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US";
+    speechSynthesis.cancel();
     speech.text = text;
     speechSynthesis.speak(speech);
   };
@@ -76,8 +77,6 @@ window.onload = function () {
   };
 
   const nextStepManual = () => {
-    readText("Skipping step");
-
     nextStep();
   };
 
@@ -108,6 +107,24 @@ window.onload = function () {
     }, 1000);
   };
 
+  // Countdown to start step
+  const countdownToStart = (maxTime) => {
+    clearInterval(timer);
+
+    timer = setInterval(() => {
+      maxTime--;
+      if (maxTime < 6 && maxTime > 0) {
+        readText(maxTime);
+      }
+
+      if (maxTime <= 0) {
+        clearInterval(timer);
+        readText("Start!");
+        initTimer(steps[currStep].duration);
+      }
+    }, 1000);
+  };
+
   // Update current step
   const updateCurrStep = () => {
     if (!steps[currStep]) return;
@@ -125,11 +142,10 @@ window.onload = function () {
         : "Next: " + steps[currStep + 1]?.name;
 
     // Read step info
-    const text = `${steps[currStep].name} - ${steps[currStep].duration} seconds`;
+    const text = `Next step: ${steps[currStep].name} - ${steps[currStep].duration} seconds`;
     readText(text);
 
-    // Init timer
-    initTimer(steps[currStep].duration);
+    countdownToStart(steps[currStep].interval);
   };
 
   // Complete workout
